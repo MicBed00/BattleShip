26a91bf8ea1b83e338db009937c8f1124a9ea3c2
Michał Będziak <michal.bedziak@gmail.com>
2022-10-26 18:54:31 +0200

    dorzucenie testów klasy Ship ifHit i ifDead

--- logic/src/test/java/ShipTest.java
+++ logic/src/test/java/ShipTest.java
@@ -1,15 +1,90 @@
+import DataConfig.Position;
+import board.Shot;
 import org.junit.jupiter.api.Test;
+import ship.Ship;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShipTest {
 
-   /*
-   Nie wiem jak przeporawdzić testy na metodzie, która jest wywoływana na instancji np. isHit() czy isColliding()
-    */
+    @Test
+    public void hittedShipOneMasted() {
+        Ship ship = new Ship(1,1,1, Position.VERTICAL);
+        Shot shot = new Shot(1, 1);
 
-//    @Test
+        assertTrue(ship.checkIfHit(shot.getX(), shot.getY()));
+    }
 
+    @Test
+    public void hittedShipThreeMasted() {
+        Ship ship = new Ship(3,5,5,Position.HORIZONTAL);
+        Shot shot = new Shot(4,5);
+
+        assertTrue(ship.checkIfHit(shot.getX(), shot.getY()));
+    }
+    @Test
+    public void hittedShipFourMasted() {
+        Ship ship = new Ship(4,9,0,Position.HORIZONTAL);
+        Shot shot = new Shot(6,0);
+
+        assertTrue(ship.checkIfHit(shot.getX(), shot.getY()));
+    }
+
+    @Test
+    public void missedShipFourMasted() {
+        Ship ship = new Ship(4,9,0,Position.HORIZONTAL);
+        Shot shot = new Shot(9,2);
+
+        assertFalse(ship.checkIfHit(shot.getX(), shot.getY()));
+    }
+
+    @Test
+    public void sunkShipOneMasted() {
+        Ship ship = new Ship(1,5,5,Position.HORIZONTAL);
+        Shot shot = new Shot(5,5);
+        ship.checkIfHit(shot.getX(), shot.getY());
+
+        assertTrue(ship.checkIfDead());
+    }
+
+    @Test
+    public void sunkShipThreeMasted() {
+        Ship ship = new Ship(3,0,5,Position.VERTICAL);
+        Shot shot = new Shot(0,5);
+        Shot shot1 = new Shot(0,6);
+        Shot shot2 = new Shot(0,7);
+        ship.checkIfHit(shot.getX(), shot.getY());
+        ship.checkIfHit(shot1.getX(), shot1.getY());
+        ship.checkIfHit(shot2.getX(), shot2.getY());
+
+        assertTrue(ship.checkIfDead());
+    }
+
+    @Test
+    public void notSunkShipThreeMasted() {
+        Ship ship = new Ship(3,0,5,Position.VERTICAL);
+        Shot shot = new Shot(0,5);
+        Shot shot1 = new Shot(0,6);
+        Shot shot2 = new Shot(5,7);
+        ship.checkIfHit(shot.getX(), shot.getY());
+        ship.checkIfHit(shot1.getX(), shot1.getY());
+        ship.checkIfHit(shot2.getX(), shot2.getY());
+
+        assertFalse(ship.checkIfDead());
+    }
+
+    @Test
+    public void notSunkShipFourMasted() {
+        Ship ship = new Ship(4,5,5,Position.VERTICAL);
+        Shot shot = new Shot(5,5);
+        Shot shot1 = new Shot(5,6);
+        Shot shot2 = new Shot(5,7);
+        ship.checkIfHit(shot.getX(), shot.getY());
+        ship.checkIfHit(shot1.getX(), shot1.getY());
+        ship.checkIfHit(shot2.getX(), shot2.getY());
+
+        assertFalse(ship.checkIfDead());
+    }
 
 
 }
