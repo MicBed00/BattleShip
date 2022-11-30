dc9ac97b2f2f9dc989c6a7d02fc0348e5db0bcaf
Michał Będziak <michal.bedziak@gmail.com>
2022-11-30 16:17:27 +0100

    Po mergu z goodBranch

--- web/src/main/java/com/web/controller/GameControllerView.java
+++ web/src/main/java/com/web/controller/GameControllerView.java
@@ -2,11 +2,9 @@ package com.web.controller;
 
 import com.web.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
