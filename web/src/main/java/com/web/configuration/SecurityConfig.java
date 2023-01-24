eadfa68210fc9f377838114a2ba8e93e14a6bcb4
Michał Będziak <michal.bedziak@gmail.com>
2023-01-24 19:34:07 +0100

    .

--- web/src/main/java/com/web/configuration/SecurityConfig.java
+++ web/src/main/java/com/web/configuration/SecurityConfig.java
@@ -18,20 +18,21 @@ public class SecurityConfig {
 //    mvcMatcher jest deprecated i zastąpiony requestMatchers
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests(requests -> requests
-//                .requestMatchers("/").permitAll()
-//                .requestMatchers("/img/**", "/styles/**").permitAll()
-//                .requestMatchers("/register", "/confirmation").permitAll()
-//                .anyRequest().authenticated()
-//        );
-//        http.formLogin(login -> login.loginPage("/login").permitAll());
+        http.authorizeHttpRequests(requests -> requests
+                .requestMatchers("/view/getParamGame").permitAll()
+                .requestMatchers("/img/**", "/styles/**").permitAll()
+                .requestMatchers("/register", "/confirmation").permitAll()
+                .anyRequest().authenticated()
+        );
+        http.formLogin(login -> login.loginPage("/login").permitAll());
         http.csrf().disable();
         return http.build();
     }
     @Bean
     public UserDetailsService userDetailsService() {
         User.UserBuilder userBuilder = User.builder();
-        UserDetails admin = userBuilder.username("superadmin").password("{noop}hard").roles("ADMIN").build();
+        String password = "{bcrypt}" + new BCryptPasswordEncoder().encode("hard");
+        UserDetails admin = userBuilder.username("superadmin").password(password).roles("ADMIN").build();
         UserDetails user1 = userBuilder.username("john").password("{noop}asdf1234").roles("USER").build();
         return new InMemoryUserDetailsManager(admin, user1);
     }
